d32935c6ae0e757c27b45f85e17df879e8ff5d51
Rodrigo <estudantecomp@gmail.com>
2018-11-20 18:31:15 -0600

    milestone here: combining lucene, inverted index with top scored apis from extractos and assymetric similarity relevance over titles. Remaining small set of average of 1.5 k answers to rank with a recall of 75% and hit@k of 97.98%. Fast: 4:40 min to run all the approach. Each query in half of a second.

--- scripts/experiment_analysis.sql
+++ scripts/experiment_analysis.sql
@@ -1,3 +1,4 @@
 select * 
 from metricsresults m
-order by m.hitk10 desc, m.mrrk10 desc
+order by id desc
+--order by m.hitk10 desc, m.mrrk10 desc
--- scripts/metricsResults_update.sql
+++ scripts/metricsResults_update.sql
@@ -2,3 +2,27 @@ ALTER TABLE public.metricsresults
     ADD COLUMN approach character varying(100);
 
 update metricsresults set approach = 'crokage';
+
+
+ALTER TABLE public.metricsresults
+    ADD COLUMN topclasses integer;
+
+update metricsresults set topclasses = 10;
+
+ALTER TABLE public.metricsresults
+    RENAME topsimilaranswersnumber TO topapisscoredpairspercent;
+
+ALTER TABLE public.metricsresults
+    ALTER COLUMN topapisscoredpairspercent TYPE integer;
+
+ALTER TABLE public.metricsresults
+    RENAME topsimilarquestionsnumber TO bm25topnresults;
+
+ALTER TABLE public.metricsresults
+    ALTER COLUMN bm25topnresults TYPE integer;
+
+ALTER TABLE public.metricsresults
+    ADD COLUMN topk integer;
+
+ALTER TABLE public.metricsresults
+    ADD COLUMN topsimilartitlespercent integer;
