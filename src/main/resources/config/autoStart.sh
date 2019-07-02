cc22d7c3136a0daddb92801ef9d53f9f975d80b1
muldon <estudantecomp@gmail.com>
2019-07-02 15:06:54 -0300

    start script

---
+++ src/main/resources/config/autoStart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash    
+echo "executing tool jar"
+toolPath="/home/rodrigo/projects/crokage-tool/target"
+export PATH=$PATH:/opt/node/bin
+export PATH=$PATH:/usr/local/bin
+java -jar $toolPath/crokage-tool.jar
