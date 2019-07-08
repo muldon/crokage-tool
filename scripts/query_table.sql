98d35ada55182ba622bf902c0f4d4116e3ee51bd
muldon <estudantecomp@gmail.com>
2019-07-08 15:13:11 -0300

    updates

---
+++ scripts/query_table.sql
@@ -0,0 +1,29 @@
+
+
+CREATE SEQUENCE public.query_id_seq;
+
+ALTER SEQUENCE public.query_id_seq
+    OWNER TO postgres;
+
+
+-- Table: public.query
+
+-- DROP TABLE public.query;
+
+CREATE TABLE public.query
+(
+    id integer NOT NULL,
+    query character varying(300) COLLATE pg_catalog."default",
+    ipaddress character varying(50) COLLATE pg_catalog."default",
+    date timestamp without time zone,
+    numberofcomposedanswers integer,
+    CONSTRAINT query_pk PRIMARY KEY (id)
+        USING INDEX TABLESPACE tablespaceso
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE tablespaceso;
+
+ALTER TABLE public.query
+    OWNER to postgres;
